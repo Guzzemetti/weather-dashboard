c66f06f70084c8aa9f78bf5138f06559f7e114a1
John <johnguzzettaiv@gmail.com>
2022-04-10 19:21:48 -0400

    Updated API variables in JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 // open weather API fetch request link and API KeyboardEvent
+var apiKey = d24820a4b09e08d1bb27ae1a68013291;
 
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=d24820a4b09e08d1bb27ae1a68013291
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
+
+// Variable to store user input
+var citySearch = document.getElementById("citySearch").value;
+
+fetch(queryURL);
